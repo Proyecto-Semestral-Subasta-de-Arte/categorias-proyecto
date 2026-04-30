d595cea398210a4d56bc04d6700d308cd5583fe3
VI-Alumno <geral.gonzalezs@duocuc.cl>
2026-04-30 09:22:17 -0400

    Desarrollo dto CategoriaRequestDTO y CategoriaResponseDTO

---
+++ src/main/java/cl/sda1085/categorias/dto/CategoriaRequestDTO.java
@@ -0,0 +1,20 @@
+package cl.sda1085.categorias.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class CategoriaRequestDTO {
+
+    @NotBlank(message = "El nombre no puede estar vacío")
+    private String nombre;
+
+    @NotBlank(message = "La descripción no puede estar vacía")
+    private String descripcion;
+
+}
---
+++ src/main/java/cl/sda1085/categorias/dto/CategoriaResponseDTO.java
@@ -0,0 +1,8 @@
+package cl.sda1085.categorias.dto;
+
+public class CategoriaResponseDTO {
+
+    private Long id;
+    private String nombre;
+    private String descripcion;
+}
