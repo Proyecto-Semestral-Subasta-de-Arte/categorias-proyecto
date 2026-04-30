9db4480d2d29b1d63b5528f8bdaae1cc70b973e1
VI-Alumno <geral.gonzalezs@duocuc.cl>
2026-04-30 09:09:33 -0400

    Desarrollo model Categoria

--- src/main/java/cl/sda1085/categorias/model/Categoria.java
+++ src/main/java/cl/sda1085/categorias/model/Categoria.java
@@ -1,6 +1,29 @@
 package cl.sda1085.categorias.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categorias")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,length = 100)
+    private String nombre;
+
+    @Column
+    private String descripcion;+
 
 
 }
