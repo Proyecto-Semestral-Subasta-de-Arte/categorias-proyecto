43109952832553581f146ce84ce83615f80060c0
VI-Alumno <VI-Alumno@duoc.acad>
2026-05-08 09:45:46 -0400

    Desarrollo CategoriaRepository

--- src/main/java/cl/sda1085/categorias/repository/CategoriaRepository.java
+++ src/main/java/cl/sda1085/categorias/repository/CategoriaRepository.java
@@ -3,5 +3,15 @@ package cl.sda1085.categorias.repository;
 import cl.sda1085.categorias.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CategoriaRepository extends JpaRepository<Categoria,Long> {
+
+
+    // Buscar por nombre exacto
+    Optional<Categoria> findByNombre (String nombre);
+
+    // Verificar si existe una categoría por nombre
+    boolean existsByNombre (String nombre);
 }
