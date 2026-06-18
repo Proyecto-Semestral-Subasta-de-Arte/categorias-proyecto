d9cba3fa0896f5e1592efc8aebb2f14f00314852
geraldinnefca <geral.gonzalezs@duocuc.cl>
2026-06-18 04:01:35 -0400

    Corrección para funcionamiento de API Gateway

--- src/main/java/cl/sda1085/categorias/assembler/CategoriaModelAssembler.java
+++ src/main/java/cl/sda1085/categorias/assembler/CategoriaModelAssembler.java
@@ -3,10 +3,12 @@ package cl.sda1085.categorias.assembler;
 import cl.sda1085.categorias.controller.CategoriaController;
 import cl.sda1085.categorias.dto.CategoriaResponseDTO;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+@Component
 public class CategoriaModelAssembler implements RepresentationModelAssembler<CategoriaResponseDTO, CategoriaResponseDTO> {
 
     @Override
